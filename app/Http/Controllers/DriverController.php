732ed454f645f6cdc4b789900aa6f70e58a64fe4
Stepan <galealesso@gmail.com>
2024-10-15 20:08:01 +0400

    Fill store bus logic

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Driver;
 use App\Http\Requests\StoreDriverRequest;
+use App\Models\Driver;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 
 class DriverController extends Controller
 {
+    public function index()
+    {
+        return Driver::all();
+    }
+
+    public function create()
+    {
+        return view('drivers.create');
+    }
+
     public function store(StoreDriverRequest $request)
     {
         $validatedData = $request->validated();
@@ -19,6 +31,11 @@ class DriverController extends Controller
         return redirect()->route('drivers.index');
     }
 
+    public function edit(Driver $driver)
+    {
+        return view('drivers.edit', compact('driver'));
+    }
+
     public function update(StoreDriverRequest $request, Driver $driver)
     {
         $validatedData = $request->validated();
@@ -26,6 +43,18 @@ class DriverController extends Controller
         $validatedData['last_name'] = strtolower($validatedData['last_name']);
 
         $driver->update($validatedData);
+
+        return redirect()->route('drivers.index');
+    }
+
+    public function destroy(Driver $driver)
+    {
+        $driver->delete();
+
+        // Отправка письма через 5 минут
+        dispatch(function () use ($driver) {
+            Mail::to($driver->email)->send(new FarewellEmail($driver));
+        })->delay(now()->addMinutes(5));
 
         return redirect()->route('drivers.index');
     }
